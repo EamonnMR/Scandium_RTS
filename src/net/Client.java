121f51d3ea9af88d9fabc61e3e780d2b7ddd6df2
Eamonn McHugh-Roohr <eamonn.m.r@gmail.com>
2014-02-09 20:53:24 -0500

    Towards a working client & server.

--- src/net/Client.java
+++ src/net/Client.java
@@ -1,5 +1,22 @@
 package net;
 
-public class Client {
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+//Simple client to test the server
+
+public class Client {
+	
+	public static void Main(String[] args) throws FileNotFoundException, IOException{
+		data.Mgr.i().loadInis();
+		int port = Integer.parseInt((String) data.Mgr.i().ports.get("0"));
+		
+		//This is mostly ripped from the 
+		
+		Socket kkSocket = new Socket("localhost", port);
+	    PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
+	    
+	}
 }
--- src/net/ConThread.java
+++ src/net/ConThread.java
@@ -1,23 +1,49 @@
 package net;
 
+import java.io.IOException;
 import java.net.Socket;
 
 public class ConThread extends Thread{
 	
-	String[] messageBuffer;
+	String messageBuffer;
 	int index;
 	Socket sock; 
 	boolean pushed = false;
+	Repeater owner;
 
-	public ConThread(Socket accept, int i, int length) {
-		messageBuffer = new String[length];
+	public ConThread(Socket sock, int index, Repeater owner) {
+		this.sock = sock;
+		this.index = index;
+		this.owner = owner;
 	}
 
-	public void pushMsg(String[] messages) {
+	public void pushMsg(String message) {
 		pushed = true;
-		for(int i = 0; i < messages.length; i++){
-			//Copy the message FIXME: Does this actually work?
-			messageBuffer[i] = messages[i].substring(0);
+		messageBuffer = message.substring(0);
+	}
+	
+	public void run(){
+		while(!sock.isClosed()){
+			if(pushed){
+				pushed = false;
+				try {
+					sock.getOutputStream().write(messageBuffer.getBytes());
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			//Read from socket, send as message
+			String msg = "";
+			try {
+				while(sock.getInputStream().available() > 0){
+					msg += Byte.toString((byte) sock.getInputStream().read());
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			owner.rcvMsg(msg,index);
 		}
 	}
 }
