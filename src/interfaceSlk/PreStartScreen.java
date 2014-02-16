48ccddc9da5eaeafc995adff23352102475f75ad
Eamonn McHugh-Roohr <eamonn.m.r@gmail.com>
2014-02-15 22:52:22 -0500

    Getting ready to make the mouse look nicer to indicate scrolling and such.

--- src/interfaceSlk/PreStartScreen.java
+++ src/interfaceSlk/PreStartScreen.java
@@ -1,12 +1,18 @@
 package interfaceSlk;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import game.PathGrid;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.tiled.TiledMap;
+
+import data.Sprite;
 
 public class PreStartScreen extends MenuState {
 
@@ -18,10 +24,17 @@ public class PreStartScreen extends MenuState {
 
 	@Override
 	public void update(GameContainer arg0, StateBasedGame arg1, int arg2)
-			throws SlickException {
+			throws SlickException{
 		// TODO Auto-generated method stub
 		TiledMap t = new TiledMap("res/RageValley.tmx");
-		game.PlayState.i().sendInfo(new game.Model(), t, new PathGrid(t));
+		try {
+			game.PlayState.i().sendInfo(new game.Model(), t, new PathGrid(t), new Sprite(
+					new Image( new FileInputStream("res/graphics/danC/mouse.png"), "0", false),
+					40, 40, 14, 1, 0, 0));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("Map loaded successfully");
 		arg1.enterState(6);
 	}
