18d1bf153f620dd34bc596e04dd7bd63e9bf3788
Eamonn McHugh-Roohr <eamonn.m.r@gmail.com>
2014-02-16 19:47:13 -0500

    Yet another bizzaro error-this time it's only using the first sprite created.

--- src/game/Model.java
+++ src/game/Model.java
@@ -1,9 +1,39 @@
 package game;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.newdawn.slick.tiled.TiledMap;
+
 public class Model {
 
-	public Model() {
-		// TODO Auto-generated constructor stub
+	private TiledMap m;
+	private PathGrid p;
+	private Map<Integer, Unit> units;
+	int unitCap = 0;
+	
+	public Model(TiledMap m, PathGrid p) {
+		this.m = m;
+		this.p = p;
+		units = new HashMap<Integer, Unit>();
 	}
 
+	public int addUnit(Unit unit) {
+		int uid = unitCap;
+		units.put(uid, unit);
+		unitCap++;
+		return uid;
+	}
+	
+	public void tickUpdate(int dt){
+		for(Integer i : units.keySet()){
+			units.get(i).updateTick(this);
+		}
+	}
+	
+	public void draw(int camX, int camY){
+		for(Integer i : units.keySet()){
+			units.get(i).draw(camX, camY);
+		}
+	}
 }
