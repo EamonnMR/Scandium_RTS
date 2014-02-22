9611ea848916a89d2fd28192c08abe9b523909f5
Eamonn McHugh-Roohr <eamonn.m.r@gmail.com>
2014-02-22 16:03:13 -0500

    Fleshing out the command system some more

--- src/commands/Command.java
+++ src/commands/Command.java
@@ -1,15 +1,52 @@
 package commands;
 
+import java.util.LinkedList;
+
 /** Commands are the key.
- * 
- * This class does two things: it forces every command to have a "toCode" method, and it has a 
- * factory that will hide the messy downcasting required to decode commands.
+ * 'orderedunits' has an array of UIDs of units being sent the command.
+ * instruction i contains the instruction itself-
  * @author Eamonn
  *
  */
-public abstract class Command {
-	public abstract String toCode();
-	public static Command fromCode(String code) {
-		return null;
+public class Command {
+	int[] orderedUnits;
+	Instruction inst;
+	public Command(int[] orderedUnits, Instruction inst) {
+		this.orderedUnits = orderedUnits;
+		this.inst = inst;
+	}
+	public Command(String code) {
+		LinkedList<Integer> unitl = new LinkedList<Integer>();
+		//Parse the units from strings...
+		String c = "";
+		String num = "";
+		while(!c.equals(":")){
+			c = code.substring(0,1);
+			code = code.substring(1);
+			if(c.equals(",")){
+				unitl.push(Integer.parseInt(num));
+			} else {
+				num += c;
+			}
+		}
+		
+		//The remainder of the code is the instruction string, we let Instruction.fromCode handle it.
+		inst = Instruction.fromCode(code);
+		
+		//Turn it into an int array
+		int[] orderedUnits = new int[unitl.size()];
+		for(int i = 0; i > unitl.size(); i ++){
+			orderedUnits[i] = unitl.get(i);
+		}
+	}
+	public String toCode(){
+		//FIXME: This could be more efficient if it used a StringBuilder rather than
+		//just a string.
+		String toSender = "";
+		for(int i : orderedUnits){
+			toSender += Integer.toString(i)+",";
+		}
+		toSender += ":";
+		return toSender +inst.toCode() + ";";
 	}
 }
---
+++ src/commands/Instruction.java
@@ -0,0 +1,14 @@
+package commands;
+
+/**
+ * This class has two distinct functions: one is to act as a base class
+ * for Instructions, and the other is to 
+ */
+
+public abstract class Instruction {
+	abstract String toCode();
+	public static Instruction fromCode(String code) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
