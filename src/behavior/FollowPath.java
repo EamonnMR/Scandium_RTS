476dc55636ec6ffe5633e6b05b7ff4768ccee664
Eamonn <Eamonn@Eagle>
2014-03-11 02:08:27 -0400

    Movement is still a bit jank and needs tightening up, but it's getting there.
    With a bit more polish it'll be fine.
    
    However, I'm now using floats in movement calculations. I'm not so sure that that's a
    good idea-perhaps I should bite the bullet and invest in a fixed-point system.

--- src/behavior/FollowPath.java
+++ src/behavior/FollowPath.java
@@ -3,20 +3,43 @@ package behavior;
 import game.Model;
 import game.Unit;
 
+import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.util.pathfinding.Path;
 
 public class FollowPath extends UnitState{
 	Path p;
-	int speed;
+	float speed;
 	int currentMove;
-	int progress;
+	float progress;
 	int curDist;
 	
-	private static final int TILE_DIST = 40;
-	private static final int DIAGONAL_DIST = 57;
+	private static final int TILE_DIST = 20;
+	private static final int DIAGONAL_DIST = 28;
 	
+	private static final int TILE_MIDDLE = 20;
 	
-	public FollowPath(Path p, int Speed){
+	/*
+	 * Vectors that point in different directions.
+	 * They're multiplied by progress to find the correct offset
+	 * at a given time (see getDirVec)
+	 * 
+	 * They're stored as constants so that they don't need to be
+	 * initialized separately at every call, and perhaps to make
+	 * the code a bit more readable.
+	 * 
+	 */
+
+	private static final float SQRT2 = 1.414f;
+	private static final Vector2f RIGHT = new Vector2f(1, 0);
+	private static final Vector2f DOWN_RIGHT = new Vector2f(SQRT2, SQRT2);
+	private static final Vector2f DOWN = new Vector2f(0, 1);
+	private static final Vector2f DOWN_LEFT = new Vector2f(-SQRT2, SQRT2);
+	private static final Vector2f LEFT = new Vector2f(-1, 0);
+	private static final Vector2f UP_LEFT = new Vector2f(-SQRT2, -SQRT2);
+	private static final Vector2f UP = new Vector2f(0,-1);
+	private static final Vector2f UP_RIGHT = new Vector2f(SQRT2,-SQRT2);
+	
+	public FollowPath(Path p, float speed){
 		this.p = p;
 		this.speed = speed;
 		currentMove = 0;
@@ -25,24 +48,48 @@ public class FollowPath extends UnitState{
 	}
 	@Override
 	public UnitState update(Model m, Unit u) {
-		/*progress += speed;
+		if(curDist == 0){
+			curDist = getDist();
+		}
+		progress += speed;
 		if(progress >= curDist){
 			if(currentMove < p.getLength() - 2){
+				progress -= curDist;
 				currentMove ++;
-				progress = 0;
+				curDist = getDist();
 			} else {
 				return new Idle();
 			}
 		}
-		*/
-		u.setFacing(getFacing());
+		int facing = getFacing();
+		u.setFacing(facing);
+		
+		Vector2f dir = getDirVec(facing);
+		
 		//Test: take one step per frame
-		u.setPos(40 * p.getX(currentMove), 40 * p.getY(currentMove));
-		currentMove++;
+		u.setPos((40 * p.getX(currentMove)) + 20 + (int)(dir.x * progress),
+				( 40 * p.getY(currentMove)) + 20 + (int)(dir.y * progress)
+				);
+		/*currentMove++;
 		if(currentMove >=p.getLength() - 1){
 			return new Idle();
-		}
+		}*/
 		return this;
+	}
+	private Vector2f getDirVec(int facing) {
+		switch(facing){
+		case 0: return RIGHT;
+		case 1: return DOWN_RIGHT;
+		case 2: return DOWN;
+		case 3: return DOWN_LEFT;
+		case 4: return LEFT;
+		case 5: return UP_LEFT;
+		case 6: return UP;
+		case 7: return UP_RIGHT;
+		}
+		//FIXME: This should be two lookup arrays, that's probably more efficient.
+		return null; //The number shall ALWYAS be within 0-7.
+		//It's ok to crash the program if it's outside that.
 	}
 	@Override
 	public void updateInterm(Model m, Unit u, int dt) {
@@ -57,6 +104,8 @@ public class FollowPath extends UnitState{
 	
 	private int getFacing(){
 		//So many ways to decode this... I'm gonna try a totally different method...
+		//Maybe I should centralize it in the code somewhere, find the most optimal method
+		//with K-maps or some such.
 		
 		int x, y;
 		x = (p.getX(currentMove) == p.getX(currentMove + 1)) ?  0 :
