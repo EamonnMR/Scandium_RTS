4905dc4a4cf30bef15c0dee4bd714162597ed718
Eamonn McHugh-Roohr <eamonn.m.r@gmail.com>
2014-03-22 20:17:05 -0400

    Beginning work on the HUD

---
+++ src/game/Hud.java
@@ -0,0 +1,44 @@
+package game;
+
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.geom.Rectangle;
+
+public class Hud {
+	public void freeUpdate(Model m){
+		
+	}
+	
+	public void render(){
+		
+	}
+	
+	public static class Button extends interfaceSlk.AbstractButton{
+		public Button(Rectangle pos) {
+			super(pos);
+		}
+
+		@Override
+		protected void doAction() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		protected void renderMouseDown(Graphics g) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		protected void renderMouseOver(Graphics g) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		protected void renderNormal(Graphics g) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+}
