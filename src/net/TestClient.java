6b28c1267f4717f92f26b20ffd003b2763a54ee1
Eamonn <Eamonn@Eagle.ads.wsc.ma.edu>
2014-04-24 11:05:15 -0400

    Adapted network test class to work with MsgTrnscv... I need to make a new server to test it properly, however.

---
+++ src/net/TestClient.java
@@ -0,0 +1,77 @@
+package net;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+//Simple client to test the server, appropriated from the java tutorials as a test...
+
+public class TestClient {
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException{
+		data.Mgr.i().loadInis();
+		int portNumber = Integer.parseInt((String) data.Mgr.i().ports.get("0"));
+		String hostName = "localhost";
+		
+		
+		//This is mostly ripped from the Java Tutorials, from Sun.
+		
+		sunsUglyTestCode(hostName, portNumber);
+	}
+	
+	/* The last remains of the Echo Client code have been... swept away.
+	 */
+	private static void sunsUglyTestCode(String hostName, int portNumber) {
+		try (
+	              /*Socket echoSocket = new Socket(hostName, portNumber);
+	    		  MessageTransceiver transcv = 
+	    			  new MessageTransceiver( new Socket(hostName, portNumber) );*/
+	              BufferedReader stdIn =
+	                  new BufferedReader(
+	                      new InputStreamReader(System.in))
+	          ) {
+	              String userInput;
+	              while ((userInput = stdIn.readLine()) != null) {
+	                  //transcv.transMsg(fmtIn(userInput));
+	                  //System.out.println(fmtOut(transcv.rcvMsg()));
+	            	  System.out.println(fmtOut(fmtIn(userInput)));
+	              }
+	          } catch (UnknownHostException e) {
+	              System.err.println("Don't know about host " + hostName);
+	              System.exit(1);
+	          } catch (IOException e) {
+	              System.err.println("Couldn't get I/O for the connection to " +
+	                  hostName);
+	              e.printStackTrace();
+	              System.exit(1);
+	        } catch (Exception e) {
+	        	e.printStackTrace();
+	        	System.exit(1);
+		} 
+	}
+	private static String fmtOut(Collection<Integer> collection) {
+		String toSender = "(" + collection.size() + ") ";
+		for(int i : collection){
+			toSender += i;
+			toSender += " ";
+		}
+		return toSender;
+	}
+
+	private static Collection<Integer> fmtIn(String userInput) {
+		List<Integer> toSender = new LinkedList<Integer>();
+		Scanner scanline = new Scanner(userInput);
+		while(scanline.hasNextInt()){
+			toSender.add(scanline.nextInt());
+		}
+		scanline.close();
+		return toSender;
+	}
+}
