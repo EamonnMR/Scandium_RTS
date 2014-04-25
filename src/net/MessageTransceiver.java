d208fe089f4abdbcde1f3289b5b83c4b9d293466
Eamonn McHugh-Roohr <eamonn.m.r@gmail.com>
2014-04-25 13:14:19 -0400

    Grudgingly refitted MessageTransciever to use strings and a scanner. It's still more efficient then the old network system, but now it's sending a string and an extra newline for each int...

--- src/net/MessageTransceiver.java
+++ src/net/MessageTransceiver.java
@@ -1,12 +1,14 @@
 package net;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+//import java.io.DataInputStream;
+//import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * This handles message sending and receiving.
@@ -18,15 +20,25 @@ import java.util.List;
  *
  */
 public class MessageTransceiver implements java.lang.AutoCloseable{
-	DataInputStream is;
-	DataOutputStream os;
+	/* DataInputStream is;
+	   DataOutputStream os;
+	*/
+	
+	Scanner is;
+	PrintWriter os;
+	
 	Socket sock;
 	public MessageTransceiver(Socket sock) throws IOException{
-		is = new DataInputStream(sock.getInputStream());
-		os = new DataOutputStream(sock.getOutputStream());
+		/*is = new DataInputStream(sock.getInputStream());
+		os = new DataOutputStream(sock.getOutputStream());*/
+		
+		os = new PrintWriter ( sock.getOutputStream() );
+		is = new Scanner( sock.getInputStream() );
 		this.sock = sock;
 	}
 	
+	/*
+	 * This version uses WriteInt
 	public void transMsg(Collection<Integer> msg) throws IOException{
 		os.writeInt(msg.size());
 		System.out.println("Sending message....");
@@ -34,14 +46,17 @@ public class MessageTransceiver implements java.lang.AutoCloseable{
 			os.writeInt(i);
 		}
 		//os.flush(); //Just in case
-	}
+	}*/
 	
-	public List<Integer> rcvMsg() throws IOException{
-		/*List<Integer> toSender = new LinkedList<Integer>();
-		for(int i = 0; i < is.readInt(); i++){
-			toSender.add(is.readInt());
-		}
-		return toSender;*/
+	/*
+	 * This version uses ReadInt
+	  public List<Integer> rcvMsg() throws IOException{
+	 
+		//List<Integer> toSender = new LinkedList<Integer>();
+		//for(int i = 0; i < is.readInt(); i++){
+		//	toSender.add(is.readInt());
+		//}
+		//return toSender;
 		
 		List<Integer> toSender = new LinkedList<Integer>();
 		System.out.println("Awaiting message");
@@ -52,8 +67,40 @@ public class MessageTransceiver implements java.lang.AutoCloseable{
 		}
 		return toSender;
 		
+	}*/
+	
+	//Ugly but currently needed hack-trying to make the thing actually transmit and rcv.
+	
+	public void transMsg(Collection<Integer> msg) throws IOException{
+		System.out.println("Sending...");
+		writeLine(msg.size());
+		for(int i : msg){
+			writeLine(i);
+		}
+		System.out.println("Done sending.");
 	}
-
+	
+	public List<Integer> rcvMsg() throws IOException{
+		
+		List<Integer> toSender = new LinkedList<Integer>();
+		System.out.println("Awaiting message");
+		int msgSize = readLine();
+		System.out.println("Reading message: Size = " + msgSize);
+		for(int i = 0; i < msgSize; i++){
+			toSender.add(readLine());
+		}
+		return toSender;
+		
+	}
+	
+	private void writeLine(int i){
+		os.println(i);
+	}
+	
+	private int readLine(){
+		return Integer.parseInt(is.nextLine());
+	}
+	
 	@Override
 	public void close() throws Exception {
 		System.out.println("Socket closed");
